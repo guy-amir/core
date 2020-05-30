fixed y_hat averaging problem :)

dl.py
@@ -27,4 +27,4 @@ def cifar_dl(prms):
     testloader = torch.utils.data.DataLoader(testset, batch_size=test_bs,
                                             shuffle=False, num_workers=4)
     
-    return trainloader,testloader
+    return trainset, testset, trainloader, testloader

model_conf.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torch
+import numpy as np
 class cifar_net(nn.Module):
     def __init__(self):
         super(cifar_net, self).__init__()
@@ -21,6 +22,7 @@ class cifar_net(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout2d(p=0.05),
@@ -32,6 +34,7 @@ class cifar_net(nn.Module):
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
@@ -39,15 +42,32 @@ class cifar_net(nn.Module):
         self.fc_layer1 = nn.Sequential(
             nn.Dropout(p=0.1),
             nn.Linear(4096, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(inplace=True),
         )
         
+        # self.fc_layer2 = nn.Sequential(
+        #     nn.Linear(1024, 512),
+            
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(p=0.1),
+        #     nn.Linear(512, 10)
+        # )
+
         self.fc_layer2 = nn.Sequential(
             nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.1),
-            nn.Linear(512, 10)
+            nn.Linear(512, 256)
         )
+
+        # self.for_tree = nn.Sequential(
+        #     nn.Linear(1024, 512),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(p=0.1),
+        #     nn.Linear(512, 10)
+        # )
 
         # self.softmax = nn.Sequential(
         #     nn.Softmax() #dim=1) #maybe add dim if necessarry
@@ -71,3 +91,143 @@ class cifar_net(nn.Module):
 
         # return x,cl1,cl2,cl3,fc1,fc2,sm #option a - smoothness testing
         return fc2 #option b - no smoothness testing
+
+class Forest(nn.Module):
+    def __init__(self, prms):
+        super(Forest, self).__init__()
+        self.trees = nn.ModuleList()
+        self.prms = prms
+        self.y_hat_avg= []
+        self.mu_list = []
+
+
+
+        #The neural network that feeds into the trees:
+        self.prenet = cifar_net()
+
+        for _ in range(self.prms.n_trees):
+            tree = Tree(prms)
+            self.trees.append(tree)
+
+    def forward(self, xb,yb=None,layer=None):
+
+        self.predictions = []
+        if self.training:
+            #convert yb from tensor to one_hot
+            yb_onehot = torch.zeros(yb.size(0), int(yb.max()+1))
+            yb = yb.view(-1,1)
+            if yb.is_cuda:
+                yb_onehot = yb_onehot.cuda()
+            yb_onehot.scatter_(1, yb, 1)
+
+            self.predictions = []            
+            self.mu = []
+            
+
+        if self.prms.use_prenet:
+            xb = self.prenet(xb)
+
+        if (self.prms.use_tree == False):
+            return xb
+
+        for tree in self.trees: 
+            
+            #construct routing probability tree:
+            mu = tree(xb)
+
+            #find the nodes that are leaves:
+            mu_midpoint = int(mu.size(1)/2)
+
+            mu_leaves = mu[:,mu_midpoint:]
+            # NL = mu_leaves.sum(1)
+            #create a normalizing factor for leaves:
+            N = mu.sum(0)
+            
+
+            if self.training:
+                if self.prms.classification:
+                    self.y_hat = yb_onehot.t() @ mu/N
+                    y_hat_leaves = self.y_hat[:,mu_midpoint:]
+                    self.y_hat_batch_avg.append(self.y_hat.unsqueeze(2))
+            ####################################################################
+            else: 
+                y_hat_val_avg = torch.cat(self.y_hat_avg, dim=2)
+                y_hat_val_avg = torch.sum(y_hat_val_avg, dim=2)/y_hat_val_avg.size(2)
+                y_hat_leaves = y_hat_val_avg[:,mu_midpoint:]
+            ####################################################################
+            pred = (mu_leaves @ y_hat_leaves.t())
+
+            self.predictions.append(pred.unsqueeze(1))
+            
+
+        ####################################################
+        # if self.training:
+        #     self.y_hat_batch_avg = torch.cat(self.y_hat_batch_avg, dim=2)
+        #     self.y_hat_batch_avg = torch.sum(self.y_hat_batch_avg, dim=2)/self.prms.n_trees
+        #     self.y_hat_avg.append(self.y_hat_batch_avg.unsqueeze(2))
+        #######################################################
+
+        self.prediction = torch.cat(self.predictions, dim=1)
+        self.prediction = torch.sum(self.prediction, dim=1)/self.prms.n_trees
+        return self.prediction
+
+class Tree(nn.Module):
+    def __init__(self,prms):
+        super(Tree, self).__init__()
+        self.depth = prms.tree_depth
+        self.n_leaf = 2 ** prms.tree_depth
+        self.n_nodes = self.n_leaf#-1
+        self.n_features = prms.features4tree
+        self.mu_cache = []
+        self.prms = prms
+
+        self.decision = nn.Sigmoid()
+
+        #################################################################################################################
+        onehot = np.eye(prms.feature_length)
+        # randomly use some neurons in the feature layer to compute decision function
+        self.using_idx = np.random.choice(prms.feature_length, self.n_leaf, replace=True)
+        self.feature_mask = onehot[self.using_idx].T
+        self.feature_mask = nn.parameter.Parameter(torch.from_numpy(self.feature_mask).type(torch.FloatTensor), requires_grad=False)
+        #################################################################################################################
+
+
+    def forward(self, x, save_flag = False):
+        if x.is_cuda and not self.feature_mask.is_cuda:
+            self.feature_mask = self.feature_mask.cuda()
+        feats = torch.mm(x.view(-1,self.feature_mask.size(0)), self.feature_mask)
+        decision = self.decision(feats) # passed sigmoid->[batch_size,n_leaf]
+
+        decision = self.decision(feats) # passed sigmoid->[batch_size,n_leaf]
+
+        decision = torch.unsqueeze(decision,dim=2) # ->[batch_size,n_leaf,1]
+        decision_comp = 1-decision
+        decision = torch.cat((decision,decision_comp),dim=2) # -> [batch_size,n_leaf,2]
+
+        mu = x.data.new(x.size(0),1,1).fill_(1.)
+        big_mu = x.data.new(x.size(0),2,1).fill_(1.)
+        begin_idx = 1
+        end_idx = 2
+        for n_layer in range(0, self.depth):
+            # mu stores the probability a sample is routed at certain node
+            # repeat it to be multiplied for left and right routing
+            mu = mu.repeat(1, 1, 2)
+            # the routing probability at n_layer
+            _decision = decision[:, begin_idx:end_idx, :] # -> [batch_size,2**n_layer,2]
+            mu = mu*_decision # -> [batch_size,2**n_layer,2]
+            begin_idx = end_idx
+            end_idx = begin_idx + 2 ** (n_layer+1)
+            # merge left and right nodes to the same layer
+            mu = mu.view(x.size(0), -1, 1)
+            big_mu = torch.cat((big_mu,mu),1)
+
+        big_mu = big_mu.view(x.size(0), -1)      
+        return big_mu #-> [batch size,n_leaf]
+
+def level2nodes(tree_level):
+    return 2**(tree_level+1)
+
+def level2node_delta(tree_level):
+    start = level2nodes(tree_level-1)
+    end = level2nodes(tree_level)
+    return [start,end]

train_conf.py
@@ -7,36 +7,11 @@ class Trainer():
 
         self.prms = prms
         self.net = net
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.SGD(net.parameters(), lr=prms.learning_rate, momentum=prms.momentum)
-
-    def fit(self,trainloader):
-        prms = self.prms
-
-        for epoch in range(prms.epochs):  # loop over the dataset multiple times
-
-            running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                # get the x; data is a list of [x, y]
-                x, y = data[0].to(prms.device), data[1].to(prms.device)
-
-                # zero the parameter gradients
-                self.optimizer.zero_grad()
-
-                # forward + backward + optimize
-                preds = self.net(x)
-                loss = self.criterion(preds, y)
-                loss.backward()
-                self.optimizer.step()
-
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                        (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+        self.criterion = nn.NLLLoss()
+        self.optimizer = optim.SGD(net.parameters(), lr=prms.learning_rate, momentum=prms.momentum, weight_decay=self.prms.weight_decay)
 
     def validation(self,testloader):
+        self.net.train(False)
         prms = self.prms
         correct = 0
         total = 0
@@ -50,3 +25,46 @@ class Trainer():
 
         print('Accuracy of the network on the 10000 test images: %d %%' % (
             100 * correct / total))
+
+    def fit(self,trainloader,testloader):
+        self.net.train(True)
+        prms = self.prms
+        self.net.y_hat_avg = []
+
+        for epoch in range(prms.epochs):  # loop over the dataset multiple times
+
+            self.net.train(True)
+            #add if for tree:
+            
+            self.net.y_hat_batch_avg = []
+
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                # get the x; data is a list of [x, y]
+                xb, yb = data[0].to(prms.device), data[1].to(prms.device)
+
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # forward + backward + optimize
+                
+                preds = self.net(xb,yb)
+                loss = self.criterion(preds, yb)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 10 == 9:    # print every 200 mini-batches
+                    print(f'[{epoch + 1}, {i + 1}] loss: {running_loss}')
+                    running_loss = 0.0
+
+            self.net.y_hat_batch_avg = torch.cat(self.net.y_hat_batch_avg, dim=2)
+            self.net.y_hat_batch_avg = torch.sum(self.net.y_hat_batch_avg, dim=2)/self.net.y_hat_batch_avg.size(2)
+            self.net.y_hat_avg.append(self.net.y_hat_batch_avg.unsqueeze(2))
+
+            self.validation(testloader)
+            
+            # self.net.y_hat_avg = torch.cat(self.net.y_hat_avg, dim=2)
+            # self.net.y_hat_avg = torch.sum(self.net.y_hat_avg, dim=2)/self.net.y_hat_avg.size(2)
+